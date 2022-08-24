5b81803b08f4f4f3a0d0ce557a49c26d6557c99a
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-08-24 15:19:21 -0400

    refac: transform jest config file in a typescript file

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
@@ -7,3 +9,5 @@ module.exports = {
     '.+\\.ts$': 'ts-jest'
   }
 }
+
+export default config

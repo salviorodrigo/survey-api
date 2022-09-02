bfe97395019ee41962ec7911f1fce80330758b1c
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-09-02 12:38:09 -0400

    feat: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -11,7 +11,7 @@ export class SignUpController {
         body: {}
       }
     }
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
     if (!thisResponse.filled) {
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {

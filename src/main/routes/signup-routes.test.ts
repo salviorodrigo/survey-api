592ca6019f54193ca9274083d36724c556a060a6
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-10-12 16:37:50 -0400

    test: add ExpressRouteAdapter

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -18,6 +18,12 @@ describe('SignUp Routes', () => {
   test('Should return 200 on success', async () => {
     await request(app)
       .post('/api/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      })
       .expect(200)
   })
 })

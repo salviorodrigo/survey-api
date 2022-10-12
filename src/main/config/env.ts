c924683f71bc0e790c71adc6db786284521bb250
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-10-12 14:48:16 -0400

    feat: add SignUp factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/survey-api',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  saltFromEncrypter: 12
 }
---
+++ src/main/factories/signup.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import env from '../config/env'
+
+export const makeSignUpController = (): SignUpController => {
+  const bcryptAdapter = new BcryptAdapter(env.saltFromEncrypter)
+  const addAccountRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, addAccountRepository)
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+
+  return signUpController
+}

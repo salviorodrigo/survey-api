66667ba388b10a72d5ba13506784e0a9dc46191d
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-10-14 15:01:43 -0400

    feat: implements LogControllerDecorator on SigUpController composition

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,16 +1,18 @@
+import { Controller } from '../../presentation/protocols'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from './../decorators/log'
 import env from '../config/env'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(env.saltFromEncrypter)
   const addAccountRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, addAccountRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-
-  return signUpController
+  const logControllerDecorator = new LogControllerDecorator(signUpController)
+  return logControllerDecorator
 }

0450d4364fddaafc026d818a34012f99fdfb116a
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-10-18 16:53:28 -0400

    feat: ensure LoginController calls EmailValidato with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,14 @@
 import { badRequest } from './../../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../../protocols'
 import { MissingParamError } from '../../errors'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const thisResponse = {
       filled: false,
@@ -12,14 +18,22 @@ export class LoginController implements Controller {
       }
     }
 
-    if (!httpRequest.body.email) {
-      thisResponse.filled = true
-      thisResponse.data = badRequest(new MissingParamError('email'))
+    if (!thisResponse.filled) {
+      if (!httpRequest.body.email) {
+        thisResponse.filled = true
+        thisResponse.data = badRequest(new MissingParamError('email'))
+      }
     }
 
-    if (!httpRequest.body.password) {
-      thisResponse.filled = true
-      thisResponse.data = badRequest(new MissingParamError('password'))
+    if (!thisResponse.filled) {
+      if (!httpRequest.body.password) {
+        thisResponse.filled = true
+        thisResponse.data = badRequest(new MissingParamError('password'))
+      }
+    }
+
+    if (!thisResponse.filled) {
+      this.emailValidator.isValid(httpRequest.body.email)
     }
 
     return thisResponse.data

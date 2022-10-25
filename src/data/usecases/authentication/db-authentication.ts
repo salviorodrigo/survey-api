5857edeebdd1f8a31cdb11d13450c1ea5aa564ca
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-10-24 23:23:57 -0400

    feat: ensure DbAuthenticator calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,15 @@
+import { Authenticator, AuthenticatorModel } from '../../../domain/usecases/authenticator'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+
+export class DbAuthenticator implements Authenticator {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth (credentials: AuthenticatorModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(credentials.email)
+    return await new Promise(resolve => resolve(null))
+  }
+}

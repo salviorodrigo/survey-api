e0b0ce58624d26cab858e9540b54c9cd92211536
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-06 11:13:31 -0400

    feat: add AddSurvey Validators factory

---
+++ src/main/factories/controllers/survey/add-survey-validator-factory.ts
@@ -0,0 +1,11 @@
+import { ValidatorComposite, RequiredFieldValidator } from '../../../../validation/validators'
+import { Validator } from '../../../../presentation/protocols'
+
+export const makeAddSurveyValidator = (): ValidatorComposite => {
+  const validators: Validator[] = []
+  for (const field of ['question', 'answers']) {
+    validators.push(new RequiredFieldValidator(field))
+  }
+
+  return new ValidatorComposite(validators)
+}

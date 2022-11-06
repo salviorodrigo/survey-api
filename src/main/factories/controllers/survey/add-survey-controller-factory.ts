b23602f13f36035cd0a1f8c98dc87115e17c4ecb
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-06 11:14:56 -0400

    feat: add AddSurvey Composition

---
+++ src/main/factories/controllers/survey/add-survey-controller-factory.ts
@@ -0,0 +1,10 @@
+import { makeDbAddSurvey } from './../../usecases/add-survey/db-add-survey-factory'
+import { makeAddSurveyValidator } from './add-survey-validator-factory'
+import { AddSurveyController } from './../../../../presentation/controllers/survey/add-survey/add-survey-controller'
+import { Controller } from '../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+
+export const makeAddSurveyController = (): Controller => {
+  const addSurveyController = new AddSurveyController(makeAddSurveyValidator(), makeDbAddSurvey())
+  return makeLogControllerDecorator(addSurveyController)
+}

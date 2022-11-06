ced6b16078021b122736020089a9d82aa865d8ec
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-06 11:16:39 -0400

    test: map AddSurvey on polls route

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,38 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+describe('Survey Routes', () => {
+  let surveyCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('polls')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /survey', () => {
+    test('Should return 204 on success', async () => {
+      await request(app)
+        .post('/api/polls')
+        .send({
+          question: 'Question 1',
+          answers: [{
+            image: 'http://localhost:5050/image-name.jpg',
+            answer: 'Answer 1'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})

0b3cf7209e5b6e75b7aae2276e7f83169eaac93c
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-06 21:47:42 -0400

    feat: ensure DbLoadAccountByToken returns an account on success

--- src/data/usecases/load-account/db-load-account-by-token.ts
+++ src/data/usecases/load-account/db-load-account-by-token.ts
@@ -1,5 +1,5 @@
+import { AccountModel } from './../../../domain/models/account'
 import { Decrypter } from '../../protocols/cryptography/decrypter'
-import { AccountModel } from '../add-account/db-add-account-protocols'
 import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
 import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
 
@@ -10,8 +10,32 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   ) {}
 
   async load (accessToken: string, role?: string): Promise<AccountModel | null> {
-    const decryptToken = await this.decrypter.decrypt(accessToken)
-    await this.loadAccountByIdRepository.loadByToken(decryptToken, role)
-    return null
+    const thisResponse = {
+      filled: false,
+      data: null
+    }
+    let decryptToken: string | null
+    let account: AccountModel | null
+
+    if (!thisResponse.filled) {
+      decryptToken = await this.decrypter.decrypt(accessToken)
+      if (!decryptToken) {
+        thisResponse.data = null
+        thisResponse.filled = true
+      }
+    }
+
+    if (!thisResponse.filled) {
+      account = await this.loadAccountByIdRepository.loadByToken(decryptToken, role)
+      if (!account) {
+        thisResponse.data = null
+        thisResponse.filled = true
+      } else {
+        thisResponse.data = account
+        thisResponse.filled = true
+      }
+    }
+
+    return thisResponse.data
   }
 }

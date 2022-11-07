f7b2fd7dd424fc49c2eb5baa82c5bad60e71dd02
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-07 15:16:29 -0400

    feat: ensure AccountMongoRepository find account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,3 +1,4 @@
+import { LoadAccountByTokenRepository } from './../../../../data/protocols/db/account/load-account-by-token-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
@@ -5,7 +6,7 @@ import { AccountModel } from '../../../../domain/models/account'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
@@ -13,8 +14,8 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
     return await new Promise(resolve => resolve(account))
   }
 
-  async loadByEmail (email: string): Promise<AccountModel> {
-    let thisResponse = null as AccountModel
+  async loadByEmail (email: string): Promise<AccountModel | null> {
+    let thisResponse = null
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.findOne({ email })
     if (result) {
@@ -33,5 +34,18 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
         $set: { accessToken: token }
       }
     )
+  }
+
+  async loadByToken (token: string, role?: string): Promise<AccountModel | null> {
+    let thisResponse = null
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.findOne({
+      accessToken: token,
+      role
+    })
+    if (result) {
+      thisResponse = MongoHelper.map(result)
+    }
+    return await Promise.resolve(thisResponse)
   }
 }

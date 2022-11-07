24ebeb497ab6673c22e6cedf6d601e1dd3c3a5f2
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-06 20:19:37 -0400

    test: ensure AuthMiddleware calls LoadAccountByToken with correrc value

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,17 +1,44 @@
+import { LoadAccountByToken } from './../../domain/usecases/load-account-by-token'
+import { AccountModel } from '../../domain/models/account'
 import { AccessDeniedError } from './../errors'
 import { forbidden } from './../helpers/http/http-helper'
 import { HttpRequest } from './../protocols/http'
 import { AuthMiddleware } from './auth-middleware'
 
+const makeFakeRequest = (): HttpRequest => ({
+  headers: {
+    'x-access-token': 'valid_token'
+  }
+})
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'hashed_value'
+})
+
+const makeLoadAccountByTokenStub = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel | null> {
+      const account = makeFakeAccount()
+      return await Promise.resolve(account)
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
+
 interface SutTypes {
   sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware()
-
+  const loadAccountByTokenStub = makeLoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
   return {
-    sut
+    sut,
+    loadAccountByTokenStub
   }
 }
 
@@ -23,5 +50,13 @@ describe('Auth Middleware', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadAccountSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(loadAccountSpy).toHaveBeenCalledWith(httpRequest.headers['x-access-token'])
   })
 })

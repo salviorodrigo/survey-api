400e34ab68651c6c37d2cbe5d885737a58299683
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-06 20:19:17 -0400

    feat: ensure AuthMiddleware calls LoadAccountByToken with correrc value

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,29 @@
 import { AccessDeniedError } from './../errors/access-denied-error'
 import { forbidden } from './../helpers/http/http-helper'
 import { Middleware, HttpRequest, HttpResponse } from '../protocols'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 
 export class AuthMiddleware implements Middleware {
+  constructor (private readonly loadAccountByTokenStub: LoadAccountByToken) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await Promise.resolve(forbidden(new AccessDeniedError()))
+    const thisResponse = {
+      filled: false,
+      data: null
+    }
+    if (!thisResponse.filled) {
+      if (!httpRequest.headers?.['x-access-token']) {
+        thisResponse.data = forbidden(new AccessDeniedError())
+      }
+    }
+
+    if (!thisResponse.filled) {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (accessToken) {
+        await this.loadAccountByTokenStub.load(accessToken)
+        thisResponse.data = null
+      }
+    }
+
+    return thisResponse.data
   }
 }

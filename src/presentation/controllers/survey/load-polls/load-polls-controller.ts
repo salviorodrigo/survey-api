01486bc30e31a4012e2a6665bf3bef0d9360285c
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-12 09:34:35 -0400

    feat: ensure LoadPollsController returns 200 on sucess

--- src/presentation/controllers/survey/load-polls/load-polls-controller.ts
+++ src/presentation/controllers/survey/load-polls/load-polls-controller.ts
@@ -1,10 +1,21 @@
 import { Controller, HttpRequest, HttpResponse, LoadPolls } from './load-polls-protocols'
+import { ok } from '../../../helpers/http/http-helper'
 
 export class LoadPollsController implements Controller {
   constructor (private readonly loadPolls: LoadPolls) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadPolls.load()
-    return await Promise.resolve(null)
+    const thisReponse = {
+      filled: false,
+      data: null
+    }
+    if (!thisReponse.filled) {
+      const polls = await this.loadPolls.load()
+      if (polls) {
+        thisReponse.data = ok(polls)
+        thisReponse.filled = true
+      }
+    }
+    return thisReponse.data
   }
 }

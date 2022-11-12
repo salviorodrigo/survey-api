7347c5b08a865b3147cdb95d4e07a7b233a4a9ad
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-12 18:37:33 -0400

    feat: ensure SurveyMongoRepository load all suverys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,17 @@
 import { AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
 import { AddSurveyModel } from './../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyModel } from '../../../../domain/models/survey'
+import { LoadPollsRepository } from '../../../../data/protocols/db/survey/load-polls-repository'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadPollsRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('polls')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('polls')
+    return await surveyCollection.find().toArray()
   }
 }

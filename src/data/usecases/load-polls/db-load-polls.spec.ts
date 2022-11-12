b0ccb8070d6c1f1c1135b85c21dd5a22097d77f0
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-12 12:22:52 -0400

    test: esure DbLoadPolls calls LoadPollsRepository

---
+++ src/data/usecases/load-polls/db-load-polls.spec.ts
@@ -0,0 +1,56 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadPollsRepository } from '../../protocols/db/survey/load-polls-repository'
+import { DbLoadPolls } from './db-load-polls'
+
+describe('DbLoadPolls Usecase', () => {
+  const makeFakePolls = (): SurveyModel[] => {
+    return [{
+      id: 'any_id',
+      question: 'any_question',
+      answers: [{
+        image: 'any_image',
+        answer: 'any_answer'
+      }, {
+        answer: 'any_anwer 2'
+      }],
+      date: new Date()
+    }, {
+      id: 'another_id',
+      question: 'another_question',
+      answers: [{
+        answer: 'another_answer'
+      }],
+      date: new Date()
+    }]
+  }
+
+  const makeLoadPollsRepositoryStub = (): LoadPollsRepository => {
+    class LoadPollsRepositoryStub implements LoadPollsRepository {
+      async loadAll (): Promise<SurveyModel[]> {
+        return await Promise.resolve(makeFakePolls())
+      }
+    }
+    return new LoadPollsRepositoryStub()
+  }
+
+  interface SutTypes {
+    sut: DbLoadPolls
+    loadPollsRepositoyStub: LoadPollsRepository
+  }
+
+  const makeSut = (): SutTypes => {
+    const loadPollsRepositoyStub = makeLoadPollsRepositoryStub()
+    const sut = new DbLoadPolls(loadPollsRepositoyStub)
+    return {
+      sut,
+      loadPollsRepositoyStub
+    }
+  }
+
+  test('Should call LoadPollsRepository', async () => {
+    const { sut, loadPollsRepositoyStub } = makeSut()
+    const loadSpy = jest.spyOn(loadPollsRepositoyStub, 'loadAll')
+    await sut.load()
+    expect(loadSpy).toHaveBeenCalled()
+  })
+})

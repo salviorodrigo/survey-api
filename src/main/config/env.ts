0c386f7ad5ddc1c19fd9186d5e4113aa39633d23
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-15 23:02:25 -0400

    refactor: allows api to run locally or with docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/survey-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/survey-api',
   port: process.env.PORT ?? 5050,
   saltFromHasher: process.env.SALT_FROM_HASHER as unknown as number ?? 12,
   jwtSecret: process.env.JWT_SECRET ?? 'secret'

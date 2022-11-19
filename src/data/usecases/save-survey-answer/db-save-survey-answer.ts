43db42136d1400a9a170b08c9b0da1a9a5a001e3
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-19 10:11:58 -0400

    feat: ensure DbSaveSurveyAnswer return a SurveyAnswerModel on success

--- src/data/usecases/save-survey-answer/db-save-survey-answer.ts
+++ src/data/usecases/save-survey-answer/db-save-survey-answer.ts
@@ -5,8 +5,18 @@ import { SaveSurveyAnswer, SaveSurveyAnswerModel } from '@/domain/usecases/save-
 export class DbSaveSurveyAnswer implements SaveSurveyAnswer {
   constructor (private readonly saveSurveyAnswerRepository: SaveSurveyAnswerRepository) {}
 
-  async save (surveyAnswerData: SaveSurveyAnswerModel): Promise<SurveyAnswerModel> {
-    await this.saveSurveyAnswerRepository.save(surveyAnswerData)
-    return null
+  async save (surveyAnswerData: SaveSurveyAnswerModel): Promise<SurveyAnswerModel | null> {
+    const thisResponse = {
+      filled: false,
+      data: null
+    }
+    if (!thisResponse.filled) {
+      const surveyAnswer = await this.saveSurveyAnswerRepository.save(surveyAnswerData)
+      if (surveyAnswer) {
+        thisResponse.data = surveyAnswer
+        thisResponse.filled = true
+      }
+    }
+    return thisResponse.data
   }
 }
--- src/domain/usecases/save-survey-answer.ts
+++ src/domain/usecases/save-survey-answer.ts
@@ -3,5 +3,5 @@ import { SurveyAnswerModel } from '@/domain/models/survey-answer'
 export type SaveSurveyAnswerModel = Omit<SurveyAnswerModel, 'id'>
 
 export interface SaveSurveyAnswer {
-  save (data: SaveSurveyAnswerModel): Promise<SurveyAnswerModel>
+  save (data: SaveSurveyAnswerModel): Promise<SurveyAnswerModel | null>
 }

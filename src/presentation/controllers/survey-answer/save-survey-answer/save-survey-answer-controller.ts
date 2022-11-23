c782162de190ca0b19164d5050d3a56d92cb6e86
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-11-23 09:33:27 -0400

    feat: ensure SaveSurveyAnswerController returns 400 e LoadSurveyById returns null

--- src/presentation/controllers/survey-answer/save-survey-answer/save-survey-answer-controller.ts
+++ src/presentation/controllers/survey-answer/save-survey-answer/save-survey-answer-controller.ts
@@ -1,10 +1,11 @@
-import { serverError } from '@/presentation/helpers/http/http-helper'
 import {
   Controller,
   HttpRequest,
   HttpResponse,
   LoadSurveyById
 } from './save-survey-answer-controller-protocols'
+import { InvalidParamError } from '@/presentation/errors'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 
 export class SaveSurveyAnswerController implements Controller {
   constructor (
@@ -17,7 +18,14 @@ export class SaveSurveyAnswerController implements Controller {
       data: null
     }
     try {
-      await this.surveyLoader.loadById(httpRequest.params.surveyId)
+      const { surveyId } = httpRequest.params
+      if (!thisResponse.filled) {
+        const survey = await this.surveyLoader.loadById(surveyId)
+        if (!survey) {
+          thisResponse.data = badRequest(new InvalidParamError('surveyId'))
+          thisResponse.filled = true
+        }
+      }
     } catch (error) {
       thisResponse.data = serverError(error)
       thisResponse.filled = true

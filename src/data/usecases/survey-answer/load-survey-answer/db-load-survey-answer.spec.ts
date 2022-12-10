8f9e8b35a58cf9acfe198c75fb32ea507b78aad0
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-12-09 22:45:40 -0400

    test: ensure DbLoadSurveyAnswer.loadAnswersBySurveyId returns SurveyAnswerModels on success

--- src/data/usecases/survey-answer/load-survey-answer/db-load-survey-answer.spec.ts
+++ src/data/usecases/survey-answer/load-survey-answer/db-load-survey-answer.spec.ts
@@ -1,5 +1,5 @@
 import { DbLoadSurveyAnswer } from './db-load-survey-answer'
-import { mockSurveyModel } from '@/domain/models/mocks'
+import { mockSurveyAnswerModel, mockSurveyModel } from '@/domain/models/mocks'
 import { LoadSurveyAnswerRepository } from '@/data/protocols/db/survey-answer'
 import { mockLoadSurveyAnswerRepository } from '@/data/protocols/db/survey-answer/mocks'
 
@@ -19,12 +19,18 @@ const makeSut = (): SutTypes => {
 
 describe('DbLoadSurveyAnswers Usecase', () => {
   describe('loadAnswersBySurveyId', () => {
-    test('Should call LoadSurveyAnswer with correct values ', async () => {
+    test('Should call LoadSurveyAnswer with correct values', async () => {
       const { sut, surveyAnswerLoaderStub } = makeSut()
       const loadSpy = jest.spyOn(surveyAnswerLoaderStub, 'loadAnswersBySurveyId')
       const fakeSurvey = mockSurveyModel()
       await sut.loadAnswersBySurveyId(fakeSurvey.id)
       expect(loadSpy).toHaveBeenCalledWith(fakeSurvey.id)
+    })
+
+    test('Should return SurveyAnswerModels on success', async () => {
+      const { sut } = makeSut()
+      const sutResponse = await sut.loadAnswersBySurveyId(mockSurveyModel().id)
+      expect(sutResponse).toEqual([mockSurveyAnswerModel()])
     })
   })
 })
